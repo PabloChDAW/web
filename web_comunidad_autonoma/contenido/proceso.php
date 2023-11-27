dec3430705a7285c45dd942aa1e714ac732b8475
PabloChDAW <pcg0031@alu.medac.es>
2023-11-27 16:25:39 +0100

    proceso php añadido al formulario

---
+++ web_comunidad_autonoma/contenido/proceso.php
@@ -0,0 +1,33 @@
+<!--El formulario llama al archivo proceso.php, haciendo echo de los datos introducidos-->
+<?php
+$nombre=$_POST['nombre'];
+echo $nombre
+
+echo <br>
+
+$email=$_POST['email'];
+echo $email
+
+echo <br>
+
+$telefono=$_POST['telefono'];
+echo $telefono
+
+echo <br>
+
+$website=$_POST['website'];
+echo $website
+
+echo <br>
+
+$asunto=$_POST['asunto'];
+echo $asunto
+
+echo <br>
+
+$mensaje=$_POST['mensaje'];
+echo $mensaje
+
+echo <br>
+
+?>
